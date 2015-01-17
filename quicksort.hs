07361b1decd7ef5c2393eaab3f2dde49de25a70f
jwvg0425 <jwvg0425@naver.com>
2015-01-18 02:55:44 +0900

    binarySearch

--- quicksort.hs
+++ quicksort.hs
@@ -3,3 +3,14 @@ quicksort [] = []
 quicksort (x:xs) = smallerSorted ++ [x] ++ biggerSorted
   where smallerSorted = quicksort [a | a <- xs, a <= x]
         biggerSorted = quicksort [a | a <- xs, a > x] 
+
+-- 해당 원소가 있는 위치를 돌려줌
+binarySearch :: (Ord a) => a -> [a] -> Int -> Int -> Int
+binarySearch _ [] _ _ = -1
+binarySearch x xs f l
+		| f > l = (-1)
+		| x == element = m
+		| x > element = binarySearch x xs (m+1) l
+		| x <= element = binarySearch x xs f (m-1)
+  where m = (f + l) `div` 2
+        element = xs !! m
